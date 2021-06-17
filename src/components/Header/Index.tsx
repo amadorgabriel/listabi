64a96a09e2f1194445cc9690df5aedff565bfe62
Gabriel Amador <gabrielramador2014@gmail.com>
2021-06-17 14:20:47 -0300

    feat: add alternativeText property into Header component

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,44 +1,54 @@
-import React from "react"
-import { View, Text, StyleSheet } from "react-native"
-import { format } from 'date-fns'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 
-import { fonts } from "../../styles/fonts"
-import { theme } from "../../styles/colors"
+import { fonts } from "../../styles/fonts";
+import { theme } from "../../styles/colors";
 
 import LogoImg from "../../assets/logo.svg";
 
-export const Header = () => {
+interface HeaderProps {
+  alternativeText?: string;
+  style?: {}
+}
+
+export const Header = ({ alternativeText,  style, ...rest}: HeaderProps) => {
   const currentDate = format(new Date(), "EEEEEE, d MMMM", {
-    locale: ptBR
+    locale: ptBR,
+  });
+
+  const styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      width: "100%",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginTop: 25,
+      marginBottom: 35,
+      ...style
+    },
+    logo: {
+      opacity: 0.9,
+    },
+    date: {
+      fontFamily: `${fonts.text}`,
+      fontSize: 18,
+      color: `${theme.colors.typografy.paragraph.secondary}`,
+      textTransform: alternativeText == undefined ? "capitalize" : "none" ,
+    },
   });
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container} {...rest}>
       <LogoImg width={25} height={25} style={styles.logo} />
 
-      <Text style={styles.date}>{currentDate}</Text>
-    </ View>
-  )
-}
+      <Text style={styles.date} >
+        {alternativeText == undefined ? currentDate : alternativeText}
+      </Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({
-  container:{
-    display: 'flex',
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginTop: 25,
-    marginBottom: 35,
-  },
-  logo: {
-    opacity: 0.9
-  },
-  date: {
-    fontFamily: `${fonts.text}`,
-    fontSize: 18,
-    color: `${theme.colors.typografy.paragraph.secondary}`,
-    textTransform: 'capitalize'
-  }
-})
+
