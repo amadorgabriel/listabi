67ec4554fa3520ed74bc8c7493ed457af747a691
Gabriel Amador <gabrielramador2014@gmail.com>
2021-06-17 16:55:40 -0300

    fix: bottom tab should hide when keyboard is opened

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Platform, TouchableOpacity, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Platform, Keyboard, TouchableOpacity, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { Feather } from "@expo/vector-icons";
@@ -9,21 +9,64 @@ import { Home } from "../pages/Home";
 import { Extract } from "../pages/Extract";
 import { SearchProduct } from "../pages/SearchProduct";
 
-import { theme } from "../styles/colors"
+import { theme } from "../styles/colors";
 
 const Tab = createBottomTabNavigator();
 
-const BottomRoutes: React.FC = () => {
+const CustomButtomComponent = ({ onPress, children}:any) => {
+  useEffect(() => {
+    Keyboard.addListener("keyboardDidShow", _keyboardDidShow);
+    Keyboard.addListener("keyboardDidHide", _keyboardDidHide);
 
+    // cleanup function
+    return () => {
+      Keyboard.removeListener("keyboardDidShow", _keyboardDidShow);
+      Keyboard.removeListener("keyboardDidHide", _keyboardDidHide);
+    };
+  }, []);
+
+  const [keyboardStatus, setKeyboardStatus] = useState('');
+  const _keyboardDidShow = () => setKeyboardStatus("Keyboard Shown");
+  const _keyboardDidHide = () => setKeyboardStatus("Keyboard Hidden");
+
+  return (
+    <TouchableOpacity
+      accessibilityRole="button"
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        paddingBottom: keyboardStatus === 'Keyboard Shown' ? 0 : 30 ,
+        display: keyboardStatus === 'Keyboard Shown' ? 'none' : 'flex'
+      }}
+      onPress={onPress}
+    >
+      <View
+        style={{
+          top: -10,
+          width: 70,
+          height: 70,
+          borderRadius: 180,
+          backgroundColor:
+            theme.colors.bottomTab.backgroundCustomButtom,
+        }}
+      >
+        {children}
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const BottomRoutes: React.FC = () => {
   return (
     <Tab.Navigator
       tabBarOptions={{
         activeTintColor: theme.colors.bottomTab.activeTintColor,
         inactiveTintColor: theme.colors.bottomTab.inactiveTintColor,
+        keyboardHidesTabBar: true,
         showLabel: false,
         style: {
           paddingBottom: 5,
-          width: '100%',
+          width: "100%",
           paddingVertical: Platform.OS === "ios" ? 20 : 0,
           height: 70,
           backgroundColor: theme.colors.bottomTab.background,
@@ -34,13 +77,9 @@ const BottomRoutes: React.FC = () => {
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({ color }) =>  (
-              <Feather
-                name="home"
-                size={30}
-                color={color}
-              />
-            )
+          tabBarIcon: ({ color }) => (
+            <Feather name="home" size={30} color={color} />
+          ),
         }}
       />
 
@@ -49,30 +88,7 @@ const BottomRoutes: React.FC = () => {
         component={SearchProduct}
         options={{
           tabBarIcon: () => <Feather name="plus" color="#FFF" size={30} />,
-          tabBarButton: ({ onPress, children }) => (
-            <TouchableOpacity
-              accessibilityRole="button"
-              style={{
-                justifyContent: "center",
-                alignItems: "center",
-                paddingBottom: 30,
-              }}
-              onPress={onPress}
-            >
-              <View
-                style={{
-                  top: -10,
-                  width: 70,
-                  height: 70,
-                  borderRadius: 180,
-                  backgroundColor: theme.colors.bottomTab.backgroundCustomButtom,
-                }}
-              >
-                {children}
-              </View>
-
-            </TouchableOpacity>
-          ),
+          tabBarButton: ({ onPress, children }) => CustomButtomComponent({ onPress, children}) 
         }}
       />
 
@@ -81,12 +97,12 @@ const BottomRoutes: React.FC = () => {
         component={Extract}
         options={{
           tabBarIcon: ({ color }) => (
-              <MaterialIcons
-                name="format-list-bulleted"
-                size={30}
-                color={color}
-              />
-            )
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={30}
+              color={color}
+            />
+          ),
         }}
       />
     </Tab.Navigator>
