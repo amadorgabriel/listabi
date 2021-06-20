9a947e0e9df93ae072a241ff146783487f222b93
Gabriel Amador <gabrielramador2014@gmail.com>
2021-06-20 19:36:10 -0300

    feat: add empty extract page

--- src/pages/Extract.tsx
+++ src/pages/Extract.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, SafeAreaView, View } from "react-native";
+import { H2, Text } from "../components/Typografy/Index";
+
+import { theme } from "../styles/colors";
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 export const Extract = () => {
   return (
-    <SafeAreaView style={styles.container}> 
-      <Text>Meu extrato</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.emptyList}>
+        <MaterialIcons name="info-outline" size={90} color="#30837F" />
+
+        <View>
+          <H2 style={styles.textCentered}>Histórico de Compras</H2>
+          <Text.Medium style={styles.textCentered}>
+            Infelizmente sua lista está vazia. Aqui você acessa todos os seus
+            últimos produtos comprados, então adicione novidades a sua lista!.
+          </Text.Medium>
+        </View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -15,5 +30,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  }
-})
+  },
+  inner: {
+    flex: 1,
+    width: "100%",
+  },
+  textCentered: {
+    textAlign: "center",
+    color: theme.colors.typografy.title.primary,
+  },
+  emptyList: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 40,
+  },
+});
