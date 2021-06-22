62ef80f66ff84d19fadcf3308e776769276dc8f2
Gabriel Amador <gabrielramador2014@gmail.com>
2021-06-22 19:41:48 -0300

    feat: add AsyncStorage

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -1,13 +1,22 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
-
-import { ProductItemProps } from "../components/ItemProduct/Index";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ProductItemProps } from "../components/ItemProduct/Add/Index";
 
 interface ProductContextData {
+  productList: ProductItemProps[];
   modalIsActive: boolean;
-  handleModal: (closeModal?: boolean) => void;
   currentProduct: ProductItemProps;
+  firstProductWasAdded: boolean;
+
+  handleModal: (closeModal?: boolean) => void;
   setCurrentProduct: (currentProduct: ProductItemProps) => void;
-  firstProductWasAdded: boolean
+  addProductToStorage: () => void;
 }
 
 interface ProductContextProviderProps {
@@ -21,7 +30,9 @@ export function ProductContextProvider({
 }: ProductContextProviderProps) {
   const [modalIsActive, setModalIsActive] = useState(false);
   const [currentProduct, setCurrentProduct] = useState({} as ProductItemProps);
-  const [firstProductWasAdded, setFirstProductWasAdded] = useState(true);
+  const [productList, setProductList] = useState([{} as ProductItemProps]);
+
+  const [firstProductWasAdded, setFirstProductWasAdded] = useState(false);
 
   function handleModal(closeModal?: boolean) {
     if (closeModal === undefined) {
@@ -31,9 +42,74 @@ export function ProductContextProvider({
     }
   }
 
+  async function setFirstProductWasAddedToStorage() {
+    try {
+      const firstProduct = Boolean(
+        await AsyncStorage.getItem("@firstProductWasAdded")
+      );
+
+      const itemList = await AsyncStorage.getItem("@myProductList");
+
+      if (firstProduct == null || firstProduct == undefined) {
+        await AsyncStorage.setItem("@firstProductWasAdded", "false");
+        setFirstProductWasAdded(false);
+      } else if (firstProduct == false || itemList != null) {
+        await AsyncStorage.setItem("@firstProductWasAdded", "true");
+        setFirstProductWasAdded(true);
+      }
+    } catch (error) {
+      throw new Error();
+    }
+  }
+
+  async function addProductToStorage() {
+    try {
+      //checka se o estado inicial da lista de produtos é um obj vazio
+      if(productList.length == 1 && !firstProductWasAdded ){
+        setProductList([currentProduct]);
+      }else{
+        setProductList([...productList, currentProduct]);
+      }
+      
+      setFirstProductWasAddedToStorage();
+
+      const jsonProduct = JSON.stringify([...productList, currentProduct]);
+      await AsyncStorage.setItem("@myProductList", jsonProduct);
+    } catch (error) {
+      throw new Error();
+    }
+  }
+
+  async function getProductListStorage() {
+    try {
+      // await AsyncStorage.clear();
+
+      const jsonValue = await AsyncStorage.getItem("@myProductList");
+
+      if (jsonValue != null) {
+        setProductList(JSON.parse(jsonValue));
+      }
+    } catch (error) {
+      throw new Error();
+    }
+  }
+
+  useEffect(() => {
+    //atualiza o estado inicial da lista com os dados já existentes no async storage
+    getProductListStorage();
+  }, []);
+
   return (
     <ProductContext.Provider
-      value={{ modalIsActive, handleModal, currentProduct, setCurrentProduct, firstProductWasAdded }}
+      value={{
+        modalIsActive,
+        handleModal,
+        currentProduct,
+        setCurrentProduct,
+        firstProductWasAdded,
+        addProductToStorage,
+        productList,
+      }}
     >
       {children}
     </ProductContext.Provider>
